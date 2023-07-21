78307ee5ade56c02daf692b52ccc80d92daa806d
Taghreed Ahmed <taghreed.ahmed@softec.ai>
2023-07-21 04:30:35 +0300

    changing sort behaviour by settimg reverse to False

--- DiscountCalculation.py
+++ DiscountCalculation.py
@@ -10,7 +10,7 @@ from Rules import get_rules
 def claculate_order_discount(order: dict, rules: List[namedtuple])-> dict:
 
     selected_discounts = list(rules | where(lambda x: x.QualifyingCondition(order)) |
-                               select(lambda y: y.DiscountRule(order)) |sort(reverse=True) | take(3))
+                               select(lambda y: y.DiscountRule(order)) |sort | take(3))
     
     final_discount = average(selected_discounts)
 
